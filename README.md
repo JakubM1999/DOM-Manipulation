9786b7808a9958047392f34c5fefdf126a1dafba
Jakub Aleksander <jakubaleksander@iMac.local>
2021-09-22 23:55:30 +0000

    fixes to js file and readme

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 <h1>Module3-DOM-Manipulation</h1>
-<h2>LINK:</h2>
+<h2>LINK: https://jakubm1999.github.io/Module3-DOM-Manipulation/Start.html</h2>
 
 <p>For this project I did one of the ideas in theme idea that I very much liked and that was: Pong video game! so I knew that I wanned to do it right away because I am a video game enthusiast so that was the perfect thing for me.</p>
 
 <p>First what I did was to create a nice lobby of start screen in other words which I think looks nice and is very clean, then loading screen which is easy to do using "setTimeout" in javaScript, then some buttons and effects on them, because these buttons lead to another page where you can play this game so I needed to create another HTML, CSS and JS files to keep things seperat where the game it self is located.</p>
 
-<p>when it came to this game I knew that I didn't know how to it from scratch so I followed a tutorial but it wasen't just copy and paste I wrote the code myself and then I had many problem, bugs and errors that I had to solve myself because there wasen't any information on the internet about these specific things also I commented and explained the code best I could.</p>
+<p>when it came to this game I knew that I didn't know how to do it from scratch so I followed a tutorial but it wasn't just copy and paste I wrote the code myself and then I had many problem, bugs and errors that I had to solve myself because there wasn't any information on the internet about these specific things also I commented and explained the code best I could.</p>
 
 <p> I learned alot by doing this project this way and I know much more about JavaScript and DOM manipulation especially how fun this is!</p>
 
-<p>Hope You like and let me know what was your score!</p>
+<p>Hope You like it and let me know what was your score!</p>
 
 <p>The game gets progressively harder ;)</p>
--- gameFunctions.js
+++ gameFunctions.js
@@ -11,7 +11,6 @@ window.onload = () => {
 
 /* Pong game */
 
-/*Posision classes */
 class Vec
 {
     constructor(x = 0, y = 0)
@@ -56,7 +55,11 @@ class Rect
         return this.pos.y + this.size.y / 2;
     }
 }
-/* Ball Rect and player Rect classes */
+/* Here I made a Ball that we are playing and the players 
+and there you can see the parameters that I created for them so
+for example: the ball class has a constructor i which are super posisions
+with "20, 20" that stands for ball's width and height same for the
+player*/
 
 class Ball extends Rect
 {
@@ -75,6 +78,11 @@ class player extends Rect
         this.score = 0;
     }
 }
+
+/* Here you can see this big class called pong that's because almost
+every function and components are in one class and one of the main
+uses for that is helps you get you code clean and it's easier to work
+that way  */
 
 class Pong
 {   /* Ball Velocity, Posision and Animation */
@@ -96,7 +104,7 @@ class Pong
         this.players.forEach(player =>{
             player.pos.y = this._canvas.height / 2;
         })
-
+        /* Here is the ball animation function*/
         let lastTime;
         const callback = (millis) => {
             if (lastTime) {
@@ -108,6 +116,18 @@ class Pong
         };
         callback();
 
+
+        /* Here is our score and what I did was to create these chars that
+        will display our score and you are maybe wondering what this matrix
+        is, it is our score. So these numbers represent pixels so 1 is white
+        and 0 is none because if you take one of these lines and you look 
+        closely you can see where the 1 are and 0 and you can make out of it 
+        actuall numbers.
+         111    010
+         101    010
+         101    010
+         101    010
+         111    010 */
         this.CHAR_PIXEL = 10;
         this.CHARS = [
             "111101101101111",
@@ -140,6 +160,13 @@ class Pong
 
         this.reset();
     }
+    /* Here we have collider which has a function so the balls doesn't ignore
+    the paddles and so it bounces off of them then where we have the math
+    function there is an function that helps the ball curve a bit when it
+    bounces so that the ball dosen't go strait all the time when you bounce it.
+    There is also this math method and what it does is that when ball baounces
+    of the paddle it encreses the speed of the ball so the game is progressively
+    harder*/
     collide(player, ball)
     {
         if (player.left < ball.right && player.right > ball.left &&
@@ -150,6 +177,11 @@ class Pong
             ball.vel.len = len * 1.05;
             }
     }
+    /* draw is the function that draws for us object and we can see
+    drawRect and score which mean the rectangles and our score. 
+    for example this ball and this player are drawn 
+    here and given a color which is only white also we have here
+    the canvas and color of our canvas */
     draw()
     {
         this._context.fillStyle = "#1a1a1a";
@@ -183,6 +215,8 @@ class Pong
             })
         })
     }
+    /* Reset allows the game to reset after scoring so the "ball"
+    dosen't float in the endless void behind our paddle */
     reset()
     {
         this._ball.pos.x = this._canvas.width / 2;
@@ -190,7 +224,10 @@ class Pong
         this._ball.vel.x = 0;
         this._ball.vel.y = 0;
     }
-
+    /* What this function does is it uses this Math random method to 
+    make a random begining of the game so the ball doesn't always go
+    the same way but it's totally random insted so it may go right or
+    left also ball.vel.len is the velocity(speed) which ball starts with*/
     start()
     {
         if(this._ball.vel.x === 0 && this._ball.vel.y === 0){
@@ -199,7 +236,8 @@ class Pong
             this._ball.vel.len = 400;
         }
     }
-/* Ball collision with walls  */
+/* Here are basiclly all of the calls to make these functions work also
+some of the functions of the ball so it may fly everywhere on the canvas.*/
     update = (dt) => {
 
         this._ball.pos.y += this._ball.vel.y * dt;
